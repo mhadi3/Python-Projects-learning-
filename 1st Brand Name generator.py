f608d5c7b125d9da300cae5c02e718d7c9016cb8
Muhammad Qamar <hadi.hfd3@gmail.com>
2026-05-22 19:29:56 +0500

    testing push

---
+++ 1st Brand Name generator.py
@@ -0,0 +1,4 @@
+print(" Welcome to the brand name generator")
+city = input("In which city you grew up?\n")
+pet = input("What is the name of your pet?\n")
+print("Your brand name could be: " + city + " " + pet)
---
+++ 5th- Password Generator.py
@@ -0,0 +1,28 @@
+import random
+
+letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+numbers = '0123456789'
+symbols = '!#$%&()*+'
+
+print("Welcome to the PyPassword Generator!")
+
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input("How many symbols would you like?\n"))
+nr_numbers = int(input("How many numbers would you like?\n"))
+
+password_list = []
+
+for _ in range(nr_letters):
+    password_list.append(random.choice(letters))
+
+for _ in range(nr_symbols):
+    password_list.append(random.choice(symbols))
+
+for _ in range(nr_numbers):
+    password_list.append(random.choice(numbers))
+
+random.shuffle(password_list)
+
+password = "".join(password_list)
+
+print(f"Your password is: {password}")
